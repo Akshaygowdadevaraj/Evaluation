36363d40877790960ec20455599b6ee74fc19b4a
sagar8088944870 <sagar.m@s.amity.edu>
2025-03-26 13:37:33 +0530

    i added case 1

---
+++ swt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+	int choice;
+    printf("Choose a department:\n");
+    printf("1. B.Tech CSE\n");
+    printf("2. AIML\n");
+    printf("3. ECE\n");
+    printf("4. Mechanical\n");
+    printf("Enter your choice (1-4): ");
+scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You chose B.Tech CSE\n");
+            break;
